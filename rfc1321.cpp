7d7fad7af37e53136a047eebe538f5045e627eb9
Michiel <m.a.van.der.vlag@hr.nl>
2018-04-25 13:44:27 +0200

    pipelined but end of msg problem

--- rfc1321.cpp
+++ rfc1321.cpp
@@ -30,32 +30,81 @@ static int rnd_seed=1;
  */
 #define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
 
-/* FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
-Rotation is separate from addition to prevent recomputation.
- */
-#define FF(a, b, c, d, x, s, ac) { \
- (a) += F ((b), (c), (d)) + (x) + (uint32_t)(ac); \
- (a) = ROTATE_LEFT ((a), (s)); \
- (a) += (b); \
-  }
+#define FF 0b00
+#define GG 0b01
+#define HH 0b10
+#define II 0b11
 
-#define GG(a, b, c, d, x, s, ac) { \
- (a) += G ((b), (c), (d)) + (x) + (uint32_t)(ac); \
- (a) = ROTATE_LEFT ((a), (s)); \
- (a) += (b); \
-  }
+static uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+                4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
 
-#define HH(a, b, c, d, x, s, ac) { \
- (a) += H ((b), (c), (d)) + (x) + (uint32_t)(ac); \
- (a) = ROTATE_LEFT ((a), (s)); \
- (a) += (b); \
-  }
+//Use binary fractional part of the sines of integers as constants:
+static uint32_t k[] = {
+	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+	0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+	0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+	0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+	0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+	0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+	0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+	0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+	0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+	0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+	0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+	0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+	0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+	0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+	0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+	0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
-#define II(a, b, c, d, x, s, ac) { \
- (a) += I ((b), (c), (d)) + (x) + (uint32_t)(ac); \
- (a) = ROTATE_LEFT ((a), (s)); \
- (a) += (b); \
-  }
+
+void md5_compress(uint32_t &a, uint32_t &b, uint32_t &c, uint32_t &d, uint32_t x[16], uint8_t index){
+#pragma HLS inline
+	/**
+	 * Determines which function we have to execute, as the functions change every 16 iterations, we only need to check 2 bits to determine which function to execute
+	 * because:
+	 * 			0b000000 = 0
+	 * 			0b010000 = 16
+	 * 			0b100000 = 32
+	 * 			0b110000 = 48
+	 * So we check the 5th nd 6th bit do determine the function
+	 */
+	uint8_t t_functionSelector = (index >> 4)&0b11;
+	//Determines which part of the message we use in the function
+	uint8_t t_number = 0;
+	//Holds the values of the function
+	uint32_t t_function = 0;
+	switch(t_functionSelector){
+	case FF:
+		t_function = F(b,c,d);
+		t_number = index;
+		break;
+	case GG:
+		t_function = G(b,c,d);
+		t_number = (1 + 5*index) %16;
+		break;
+	case HH:
+		t_function = H(b,c,d);
+		t_number = (5 + 3*index) %16;
+		break;
+	case II:
+		t_function = I(b,c,d);
+		t_number = (7*index) %16;
+		break;
+	}
+	t_function = a + x[t_number] + k[index];
+	t_function = ROTATE_LEFT(t_function,r[index]);
+
+	//Switch the inputs around a= d, b =a, c= b, d= c
+	uint32_t tmp = d;
+	d = c;
+	c = b;
+	b += t_function;
+	a = tmp;
+
+}
 
 void md5_hasher(uint32_t x[16], uint8_t msgColl){
 
@@ -79,11 +128,12 @@ void md5_hasher(uint32_t x[16], uint8_t msgColl){
 
 //	uint8_t msg2Hash[64] = "I just need a message that's 64B long such to hash 512b";
 
-//	uint32_t units=1;
+	uint32_t units=1;
 
 	uint32_t a = h0, b = h1, c = h2, d = h3;//, x[16];
 
-//	for(uint8_t hw=0;hw<units;hw++){
+	for(uint8_t hw=0;hw<units;hw++){
+#pragma HLS unroll
 
 		for (uint8_t i=0;i<14;i++){
 			int randval = rand_int();
@@ -97,78 +147,18 @@ void md5_hasher(uint32_t x[16], uint8_t msgColl){
 		x[14]=0x1B8;
 		x[15]=0;
 
+		//reversed is original message!
+				printf("\n reversed message1:\n");
+				//everything upto the 0x80 is hashed. check online for hexhasher.
+				for (uint8_t i=0;i<16;i++){
+					printf("%08x",((x[i] & 0xFF)<<3*8) | ((x[i] & 0xFF00) << 8) | ((x[i] & 0xFF0000) >> 8) | ((x[i] & 0xFF000000) >> 3*8));
+				}
 
-		/* Round 1 */
-		FF (a, b, c, d, x[ 0], S11, 0xd76aa478); /* 1 */
-		FF (d, a, b, c, x[ 1], S12, 0xe8c7b756); /* 2 */
-		FF (c, d, a, b, x[ 2], S13, 0x242070db); /* 3 */
-		FF (b, c, d, a, x[ 3], S14, 0xc1bdceee); /* 4 */
-		FF (a, b, c, d, x[ 4], S11, 0xf57c0faf); /* 5 */
-		FF (d, a, b, c, x[ 5], S12, 0x4787c62a); /* 6 */
-		FF (c, d, a, b, x[ 6], S13, 0xa8304613); /* 7 */
-		FF (b, c, d, a, x[ 7], S14, 0xfd469501); /* 8 */
-		FF (a, b, c, d, x[ 8], S11, 0x698098d8); /* 9 */
-		FF (d, a, b, c, x[ 9], S12, 0x8b44f7af); /* 10 */
-		FF (c, d, a, b, x[10], S13, 0xffff5bb1); /* 11 */
-		FF (b, c, d, a, x[11], S14, 0x895cd7be); /* 12 */
-		FF (a, b, c, d, x[12], S11, 0x6b901122); /* 13 */
-		FF (d, a, b, c, x[13], S12, 0xfd987193); /* 14 */
-		FF (c, d, a, b, x[14], S13, 0xa679438e); /* 15 */
-		FF (b, c, d, a, x[15], S14, 0x49b40821); /* 16 */
 
-		/* Round 2 */
-		GG (a, b, c, d, x[ 1], S21, 0xf61e2562); /* 17 */
-		GG (d, a, b, c, x[ 6], S22, 0xc040b340); /* 18 */
-		GG (c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
-		GG (b, c, d, a, x[ 0], S24, 0xe9b6c7aa); /* 20 */
-		GG (a, b, c, d, x[ 5], S21, 0xd62f105d); /* 21 */
-		GG (d, a, b, c, x[10], S22,  0x2441453); /* 22 */
-		GG (c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
-		GG (b, c, d, a, x[ 4], S24, 0xe7d3fbc8); /* 24 */
-		GG (a, b, c, d, x[ 9], S21, 0x21e1cde6); /* 25 */
-		GG (d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
-		GG (c, d, a, b, x[ 3], S23, 0xf4d50d87); /* 27 */
-		GG (b, c, d, a, x[ 8], S24, 0x455a14ed); /* 28 */
-		GG (a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
-		GG (d, a, b, c, x[ 2], S22, 0xfcefa3f8); /* 30 */
-		GG (c, d, a, b, x[ 7], S23, 0x676f02d9); /* 31 */
-		GG (b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
-
-		/* Round 3 */
-		HH (a, b, c, d, x[ 5], S31, 0xfffa3942); /* 33 */
-		HH (d, a, b, c, x[ 8], S32, 0x8771f681); /* 34 */
-		HH (c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
-		HH (b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
-		HH (a, b, c, d, x[ 1], S31, 0xa4beea44); /* 37 */
-		HH (d, a, b, c, x[ 4], S32, 0x4bdecfa9); /* 38 */
-		HH (c, d, a, b, x[ 7], S33, 0xf6bb4b60); /* 39 */
-		HH (b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
-		HH (a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
-		HH (d, a, b, c, x[ 0], S32, 0xeaa127fa); /* 42 */
-		HH (c, d, a, b, x[ 3], S33, 0xd4ef3085); /* 43 */
-		HH (b, c, d, a, x[ 6], S34,  0x4881d05); /* 44 */
-		HH (a, b, c, d, x[ 9], S31, 0xd9d4d039); /* 45 */
-		HH (d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
-		HH (c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
-		HH (b, c, d, a, x[ 2], S34, 0xc4ac5665); /* 48 */
-
-		/* Round 4 */
-		II (a, b, c, d, x[ 0], S41, 0xf4292244); /* 49 */
-		II (d, a, b, c, x[ 7], S42, 0x432aff97); /* 50 */
-		II (c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
-		II (b, c, d, a, x[ 5], S44, 0xfc93a039); /* 52 */
-		II (a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
-		II (d, a, b, c, x[ 3], S42, 0x8f0ccc92); /* 54 */
-		II (c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
-		II (b, c, d, a, x[ 1], S44, 0x85845dd1); /* 56 */
-		II (a, b, c, d, x[ 8], S41, 0x6fa87e4f); /* 57 */
-		II (d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
-		II (c, d, a, b, x[ 6], S43, 0xa3014314); /* 59 */
-		II (b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
-		II (a, b, c, d, x[ 4], S41, 0xf7537e82); /* 61 */
-		II (d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
-		II (c, d, a, b, x[ 2], S43, 0x2ad7d2bb); /* 63 */
-		II (b, c, d, a, x[ 9], S44, 0xeb86d391); /* 64 */
+		compressLoop:for(int i = 0; i < 64; i++){
+#pragma HLS PIPELINE
+			md5_compress(a, b, c, d, x, i);
+		}
 
 		h0 += a;
 		h1 += b;
@@ -176,25 +166,25 @@ void md5_hasher(uint32_t x[16], uint8_t msgColl){
 		h3 += d;
 
 		//reversing done by software
-//		x[14] = ((h0 & 0xFF)<<3*8) | ((h0 & 0xFF00) << 8) | ((h0 & 0xFF0000) >> 8) | ((h0 & 0xFF000000) >> 3*8);
-//		x[15] = ((h1 & 0xFF)<<3*8) | ((h1 & 0xFF00) << 8) | ((h1 & 0xFF0000) >> 8) | ((h1 & 0xFF000000) >> 3*8);
-//		x[16] = ((h2 & 0xFF)<<3*8) | ((h2 & 0xFF00) << 8) | ((h2 & 0xFF0000) >> 8) | ((h2 & 0xFF000000) >> 3*8);
-//		x[17] = ((h3 & 0xFF)<<3*8) | ((h3 & 0xFF00) << 8) | ((h3 & 0xFF0000) >> 8) | ((h3 & 0xFF000000) >> 3*8);
-//
-//		printf("\nhash=%08x%08x%08x%08x\n",x[14],x[15],x[16],x[17]);
+		x[14] = ((h0 & 0xFF)<<3*8) | ((h0 & 0xFF00) << 8) | ((h0 & 0xFF0000) >> 8) | ((h0 & 0xFF000000) >> 3*8);
+		x[15] = ((h1 & 0xFF)<<3*8) | ((h1 & 0xFF00) << 8) | ((h1 & 0xFF0000) >> 8) | ((h1 & 0xFF000000) >> 3*8);
+		x[16] = ((h2 & 0xFF)<<3*8) | ((h2 & 0xFF00) << 8) | ((h2 & 0xFF0000) >> 8) | ((h2 & 0xFF000000) >> 3*8);
+		x[17] = ((h3 & 0xFF)<<3*8) | ((h3 & 0xFF00) << 8) | ((h3 & 0xFF0000) >> 8) | ((h3 & 0xFF000000) >> 3*8);
+
+		printf("\nhash=%08x%08x%08x%08x\n",x[14],x[15],x[16],x[17]);
 
 		//reversed is original message!
-//		printf("\n reversed message:\n");
-//		//everything upto the 0x80 is hashed. check online for hexhasher.
-//		for (uint8_t i=0;i<16;i++){
-//			printf("%08x",((x[i] & 0xFF)<<3*8) | ((x[i] & 0xFF00) << 8) | ((x[i] & 0xFF0000) >> 8) | ((x[i] & 0xFF000000) >> 3*8));
-//		}
+		printf("\n reversed message2:\n");
+		//everything upto the 0x80 is hashed. check online for hexhasher.
+		for (uint8_t i=0;i<16;i++){
+			printf("%08x",((x[i] & 0xFF)<<3*8) | ((x[i] & 0xFF00) << 8) | ((x[i] & 0xFF0000) >> 8) | ((x[i] & 0xFF000000) >> 3*8));
+		}
 
 		//write outputs when hash is found
 		if (h0==hash2Crack0&&h1==hash2Crack2&&h2==hash2Crack2&&h3==hash2Crack3){
-			msgColl=255;
+			msgColl=hw;
 		}
-//	}
+	}
 }
 
 uint32_t need_seed (void)
